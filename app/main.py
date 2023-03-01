c86b6df54ce470e451c773b4749d372d3d93878c
sathvik-85 <69890523+sathvik-85@users.noreply.github.com>
2023-03-01 12:05:18 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -72,7 +72,8 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     return encoded_jwt
 
 def hash_func(plain_pass, hashed_pass,salt) -> bool:
-    plain_pass_hashed = bcrypt.hashpw(plain_pass.encode(), salt)
+    plain_pass_bytes = plain_pass.encode('utf-8')
+    plain_pass_hashed = bcrypt.hashpw(plain_pass_bytes, salt)
     if plain_pass_hashed == hashed_pass:
         return True
 
