eac038a800713beabda3fb7cae5dc4f5991bc358
root <root@ip-10-10-234-158.eu-west-1.compute.internal>
2023-03-05 19:31:11 +0000

    updated file

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 import os
+import io
 import time
 import bcrypt
 import pickle
 import face_recognition
+import numpy as np
+from PIL import Image
 from pymongo import MongoClient
 from datetime import datetime, timedelta
 from jose import jwt,JWTError
 from typing import Union
 from dotenv import load_dotenv
 from os.path import join,dirname
-from fastapi import FastAPI,Depends,HTTPException,status,Form,Request,UploadFile
+from fastapi import FastAPI,Depends,HTTPException,status,Form,Request,UploadFile,File
 from pydantic import BaseModel
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
@@ -78,30 +81,54 @@ def hash_func(plain_pass, hashed_pass,salt) -> bool:
 def serialize(known_face_encoding):
     return pickle.dumps(known_face_encoding)
 
+async def file_to_nparray(file):
+    file_content = await file.read()
+    pil_image = Image.open(io.BytesIO(file_content))
+    return np.array(pil_image)
 
 
 @app.post("/token", response_model = Token)
-async def user_register(formData:OAuth2PasswordRequestForm = Depends()):
-    user = collection.find_one({"username":formData.username})
-    if not user:
-        raise  HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    pass_verified = hash_func(formData.password,user["password"],user["salt"])
-    if pass_verified:
-        access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
-        access_token = create_access_token(
-        data={"sub": user["username"]}, expires_delta=access_token_expires)
-        
-        return {"access_token":access_token, "token_type":"Bearer"}
+async def user_register(img:bool,username:str,password:str,file:UploadFile=File(...)):
+    user = collection.find_one({"username":username})
+    if not img:
+
+        if not user:
+            raise  HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        pass_verified = hash_func(formData.password,user["password"],user["salt"])
+        if pass_verified:
+            access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+            access_token = create_access_token(
+            data={"sub": user["username"]}, expires_delta=access_token_expires)
+            
+            return {"access_token":access_token, "token_type":"Bearer"}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
     else:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+        unknown_img = await file_to_nparray(file)
+        unknown_face_encoding = face_recognition.face_encodings(unknown_img)[0]
+        known_face_encoding = pickle.loads(user["known_encoding"])
+        result = face_recognition.compare_faces([known_face_encoding], unknown_face_encoding)
+        if result[0] ==True:
+            access_token = create_access_token(
+            data={"sub": user["username"]}, expires_delta=access_token_expires)
+            return {"access_token":access_token,"token_type":"Bearer"}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Face Not Recognised",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        
+
+
 
     
 @app.get("/private")
@@ -118,11 +145,11 @@ async def user_home():
 
 
 @app.post("/register")
-async def user_register(file:UploadFile,username:str = Form(), password:str = Form()):
+async def user_register(file:UploadFile=File(...),username:str = Form(), password:str = Form()):
+    known_img = await file_to_nparray(file)
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
     response = collection.find_one({"username":username})
-    known_img = face_recognition.load_image_file("sharonstone1.jpg")
     known_face_encoding = face_recognition.face_encodings(known_img)[0]
     serialized_encoding = serialize(known_face_encoding)
     if not response:
